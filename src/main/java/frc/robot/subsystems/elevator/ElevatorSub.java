c2dc415fd248987ba90e040ed9131ff11d0f83ba
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2021-11-08 09:46:57 -0800

    Just wanted to save some changes

---
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSub.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems.elevator;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ElevatorSub extends SubsystemBase {
+    
+    public ElevatorSub() {
+
+    }
+}
